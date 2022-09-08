5ec0fe196471fd959376e8ebc52523543732c320
mohammad <mohammadadel8727@gmail.com>
2022-09-08 12:57:30 +0200

    added the week's challange

---
+++ week-2/challange.js
@@ -0,0 +1,9 @@
+// Variable Challenge
+let pagTitle = "Elzero", pagDescription = "Elzero Web School", pagDate = "25/10";
+let card =`<div>
+<h3>Hello ${pagTitle}</h3>
+<p>${pagDescription}</p>
+<span>${pagDate}</span>
+</div>`;
+
+document.write(card.repeat(4));
